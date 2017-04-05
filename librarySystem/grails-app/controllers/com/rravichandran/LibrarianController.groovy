37364e70a9c398f1b00f151e70d8722e5e704943
RajR1996 <b4025068@my.shu.ac.uk>
2017-04-05 02:51:48 +0100

    Search form and results page is working for librarians

--- librarySystem/grails-app/controllers/com/rravichandran/LibrarianController.groovy
+++ librarySystem/grails-app/controllers/com/rravichandran/LibrarianController.groovy
@@ -4,26 +4,46 @@ class LibrarianController {
 
 	def scaffold= Librarian
 
+	def search(){
+	}
+
+	def results(){
+	
+		def studentProps = Student.metaClass.properties*.name
+		def students = Student.withCriteria{
+			"${params.queryType}"{
+				params.each {field,value ->
+					if(studentProps.grep(field) && value){
+						ilike(field, value)
+					}
+				}
+		}
+	}
+
+	[students:students]
+	}	
+	
+
 	def login(){
 	}
 
 	def validate(){
 	
 	def user = Librarian.findByUserName(params.username)
-	if (user && user.password == params.password){
-	session.user=user
-	render view:'home'
+		if (user && user.password == params.password){
+		session.user=user
+		render view:'home'
 	}
 
-	else{
-	flash.message="Invalid username and password"
-	render view:'login'
-	}
+		else{
+			flash.message="Invalid username and password"
+			render view:'login'
+			}
 	}
 
 	def logout = {
-	session.user = null
-	redirect(uri:'/')
+		session.user = null
+		redirect(uri:'/')
 	}
 	
 
